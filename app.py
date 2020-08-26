2e6503f2f77f28269cd3d9e2c968518aa3515ea3
Isaac51743 <m15828509035@163.com>
2020-08-26 10:02:17 -0400

    8/26/2020

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ def after_request(response):
 def follow(follower, followee):
     sql1 = "select users_id from users where name = '?';"
     sql2 = "select users_id from users where name = '?';"
+    sql3 = "insert follow_relationship values (?,?)；"
     c = g.db.cursor()
     c.execute(sql1, followee)
     followee_id = str(c.fetchall()[0])
     c.execute(sql2, follower)
     follower_id = str(c.fetchall()[0])
 
-    sql3 = "insert follow_relationship values (?,?)；"
     c.execute(sql3, (follower_id, followee_id))
     g.db.commit()
 
@@ -37,13 +37,13 @@ def follow(follower, followee):
 def unfollow(follower, followee):
     sql1 = "select users_id from users where name = '?';"
     sql2 = "select users_id from users where name = '?';"
+    sql3 = "delete from follow_relationship where follower=?and followee=?;"
     c = g.db.cursor()
     c.execute(sql1, followee)
     followee_id = str(c.fetchall()[0])
     c.execute(sql2, follower)
     follower_id = str(c.fetchall()[0])
 
-    sql3 = "delete from follow_relationship where follower=?and followee=?;"
     c.execute(sql3, (follower_id, followee_id))
     g.db.commit()
 
@@ -51,35 +51,27 @@ def unfollow(follower, followee):
 @app.route('/get_follower/<string:user_name>', methods=['GET'])
 def get_follower(user_name):
     sql1 = "select users_id from users where name = '?';"
+    sql2 = "select name from users where users_id in (select follower from follow_relationship where followee = ?);"
     c = g.db.cursor()
     c.execute(sql1, user_name)
-        followee_id = str(cursor.fetchone()[0])
-        sql2 = "select name from users where users_id in (select follower from follow_relationship where followee = ?);"
-        cursor.execute(sql2, followee_id)
-        follower = cursor.fetchall()
-        return jsonify(follower)
-    except:
-        print("Error: unable to fetch data")
+    followee_id = str(c.fetchone()[0])
 
-    conn.close()
-
+    c.execute(sql2, followee_id)
+    follower = c.fetchall()
+    return jsonify(follower)
 
 
 @app.route('/get_following/<string:user_name>/<int:page>', methods=['GET'])
 def get_following(user_name, page): # page number starts from 0
-    conn, cursor = connect()
     sql1 = "select users_id from users where name = '?';"
-    try:
-        cursor.execute(sql1, user_name)
-        follower_id = str(cursor.fetchone()[0])
-        sql = "select name from users where users_id in (select followee from follow_relationship where follower = ?) order by name;"
-        cursor.execute(sql, follower_id)
-        following = cursor.fetchall()
-        return jsonify(following[page * 20:page * 20 + 20])
-    except:
-        print("Error: unable to fetch data")
+    sql2 = "select name from users where users_id in (select followee from follow_relationship where follower = ?) order by name;"
+    c = g.db.cursor()
+    c.execute(sql1, user_name)
+    follower_id = str(c.fetchone()[0])
 
-    conn.close()
+    c.execute(sql2, follower_id)
+    following = c.fetchall()
+    return jsonify(following[page * 20:page * 20 + 20])
 
 
 if __name__ == '__main__':
